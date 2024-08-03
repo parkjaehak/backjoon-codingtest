0f10f5e8a835d4a752926aa0c06b07eb45f1b8ca
parkjaehak <atom8426@gmail.com>
2024-08-03 15:32:15 +0900

    14502 실험실

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         //Gugudan2739 gugudan = new Gugudan2739();
-        Queue18258 queue18258 = new Queue18258();
+        //Queue18258 queue18258 = new Queue18258();
+        Laboratory14502 laboratory14502 = new Laboratory14502();
     }
 }
--- src/Queue18258.java
+++ src/Queue18258.java
@@ -6,21 +6,21 @@ public class Queue18258 {
     static int size = 0;
     static int front = -1;
     static int back = -1;
-    static int[] queue = new int[2000000];	// 명령의 수는 2,000,000을 안넘음
+    static int[] queue = new int[2000000];
 
 
     public Queue18258() throws IOException {
         // InputStreamReader와 BufferedReader를 사용하여 빠르게 입력을 읽음
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int inputNumber = Integer.parseInt(br.readLine());
-        if(inputNumber<1||inputNumber>2000000)
+        if (inputNumber < 1 || inputNumber > 2000000)
             return;
 
         // 출력 버퍼
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        for(int i=0; i<inputNumber;i++) {
+        for (int i = 0; i < inputNumber; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String menu = st.nextToken();
 
@@ -28,7 +28,7 @@ public class Queue18258 {
                 case "push":
                     push(Integer.parseInt(st.nextToken()));
                     break;
-                case "pop" :
+                case "pop":
                     sb.append(pop()).append("\n");
                     break;
                 case "size":
@@ -51,19 +51,20 @@ public class Queue18258 {
         br.close();
     }
 
-    public void push(int number) {
+    public static void push(int number) {
         if (front == -1 && back == -1) {
-            front++; back ++;
-        }
-        else {
+            front++;back++;
+        } else {
             back++;
         }
         queue[back] = number;
         size++;
     }
 
-    public int pop() {
+    public static int pop() {
         if (size <= 0) {
+            front = -1;
+            back = -1;
             return -1;
         }
         int pop = queue[front++];
@@ -71,25 +72,29 @@ public class Queue18258 {
         return pop;
     }
 
-    public int size() {
+    public static int size() {
         if (back < front || (back == -1 && front == -1)) {
             return 0;
         }
         return back - front + 1;
     }
 
-    public int empty() {
-        if(size()<=0)
+    public static int empty() {
+        if (size() <= 0)
             return 1;
         else
             return 0;
     }
 
-    public int front() {
+    public static int front() {
+        if (size() <= 0)
+            return -1;
         return queue[front];
     }
 
-    public int back() {
+    public static int back() {
+        if (size() <= 0)
+            return -1;
         return queue[back];
     }
 }

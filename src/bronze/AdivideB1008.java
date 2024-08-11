aadf268934952b7c38cf01e71d606f9cfc98e94d
parkjaehak <atom8426@gmail.com>
2024-08-11 14:16:05 +0900

    1008 A/B

---
+++ src/bronze/AdivideB1008.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class AdivideB1008 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1 = scanner.nextInt();
+        double num2 = scanner.nextInt();
+
+        System.out.println(num1/num2);
+    }
+}
---
+++ src/gold/Laboratory3_17142.java
@@ -0,0 +1,8 @@
+package gold;
+
+public class Laboratory3_17142 {
+
+    public static void main(String[] args) {
+
+    }
+}
